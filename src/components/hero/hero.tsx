7774a974ba6bd0d4e2155f6ba93c16d3cd9f6a2d
Scynes <dustin.greyfield@gmail.com>
2024-07-31 19:05:53 -0400

    add/refactor: The hero image not displays in a smaller format on mobile devices within a separate layout.

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -8,13 +8,18 @@ import Image from 'next/image';
 export const Hero = () => {
     return (
         <section className={ 'relative h-full bg-blue-400' }>
-            <div className={ `transition-all sm:slant-section clip absolute px-12 py-24 h-full max-w-[38rem] w-full z-10 text-white bg-gradient-to-tr from-sky-900 via-sky-700 to-yellow-300` }>
-                <Image src="/images/logo.png" alt="Logo" width={ 225 } height={ 225 } className={ 'mb-7' }/>
-                <div className={ 'mb-4' }>
-                    <p className={ 'text-4xl font-extrabold' }>STAY IN TOUCH</p>
-                    <p className={ 'text-4xl font-extrabold' }>WITH SAMANTHA</p>
+            <div className={ `flex flex-col justify-center transition-all sm:slant-section clip absolute sm:px-12 sm:py-24 h-full max-w-[38rem] w-full z-10 text-white bg-gradient-to-tr from-sky-900 via-sky-700 to-yellow-300` }>
+                <div className={ 'relative flex-1 sm:hidden' }>
+                    <Image src="/images/hero.png" alt="Hero" fill className='object-cover' />
                 </div>
-                <HeroForm />
+                <div className={ 'p-12 sm:p-0' }>
+                    <Image src="/images/logo.png" alt="Logo" width={ 225 } height={ 225 } className={ 'mb-7' }/>
+                    <div className={ 'mb-4' }>
+                        <p className={ 'text-4xl font-extrabold' }>STAY IN TOUCH</p>
+                        <p className={ 'text-4xl font-extrabold' }>WITH SAMANTHA</p>
+                    </div>
+                    <HeroForm />
+                </div>
             </div>
             <Image src="/images/hero.png" alt="Hero" fill className='object-cover' />
         </section>
